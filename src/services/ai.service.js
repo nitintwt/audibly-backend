cfd68bec290105ee985fe93628dd6402240fb6dd
Nitin Singh <nitintwt27@gmail.com>
2025-03-21 23:29:50 +0530

    convert podcast script into speaker A and Speaker B audio using kokoro TTS model  and merge it using ffmpeg library

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -5,6 +5,14 @@ const createPodcastDraft = async (text)=>{
   const prompt = `
    Convert the following text into a podcast-style conversation between two intellectual speakers. 
    Speaker A is male and Speaker B is female. Each speaker should provide thoughtful insights about the topic.
+  
+  Output Format should only be one array , and that array will be array of ojects and no nested arrays , and no other text. For example:
+  [
+    {"speaker": "A", "text": "Hey, did you hear about the new AI tool?"},
+    {"speaker": "B", "text": "Yeah! It’s really impressive. How does it work?"},
+    {"speaker": "A", "text": "Well, it uses deep learning to generate human-like conversations."},
+    {"speaker": "B", "text": "That sounds amazing! Can you give me an example?"},
+  ]
   `
   try {
     const response = await client.chatCompletion({
